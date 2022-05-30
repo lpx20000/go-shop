0e98a0dd2a6cb8d707a457795113e270a5be58c4
lpx20000 <849400324@qq.com>
2022-05-30 08:57:56 +0800

    Update appHelp.go
    get client ip

--- models/appHelp.go
+++ models/appHelp.go
@@ -1,6 +1,10 @@
 package models
 
-import "github.com/jinzhu/gorm"
+import (
+	"github.com/gin-gonic/gin"
+	"strings"
+	"github.com/jinzhu/gorm"
+)
 
 type WxappHelp struct {
 	HelpId  uint   `json:"help_id"`
@@ -21,3 +25,25 @@ func GetHelp() ([]*WxappHelp, error) {
 	}
 	return appHelp, nil
 }
+
+func GetClientIP(c *gin.Context) string {
+	ClientIP := c.ClientIP()
+	//fmt.Println("ClientIP:", ClientIP)
+	RemoteIP, _ := c.RemoteIP()
+	//fmt.Println("RemoteIP:", RemoteIP)
+	ip := c.Request.Header.Get("X-Forwarded-For")
+	if strings.Contains(ip, "127.0.0.1") || ip == "" {
+		ip = c.Request.Header.Get("X-real-ip")
+	}
+	if ip == "" {
+		ip = "127.0.0.1"
+	}
+	if RemoteIP.String() != "127.0.0.1" {
+		ip = RemoteIP.String()
+	}
+	if ClientIP != "127.0.0.1" {
+		ip = ClientIP
+	}
+	return ip
+}
+
